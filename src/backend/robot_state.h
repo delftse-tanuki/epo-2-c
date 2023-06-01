9bcadd403b9820068f1cd02c952c3370b04ae3e6
tanukibouwer <qudicalgames@gmail.com>
2023-06-01 12:14:25 +0200

    Updates to robot state struct

--- src/backend/robot_state.h
+++ src/backend/robot_state.h
@@ -8,10 +8,22 @@
 #include "uart/UARTInstructions.h"
 
 struct RobotState {
+    // What way is the robot currently facing?
     enum Facing facing;
+    // What's the next instruction we'll be sending to the robot?
     enum Instruction next_instruction;
+    // What's the robot current position? (i.e. between which crossings is it?)
     struct PointConnection current_position;
+    // Did we receive a byte from the robot in the last process loop?
     bool data_received;
+    // Did we suffer a major failure that requires a reset?
+    bool major_failure;
+    // The mines that the robot has detected so far.
+    struct PointConnection mines[100];
+    // How many mines have we found?
+    int mines_count;
+    // The path the robot is currently following.
+    struct Path current_path;
 };
 
 #endif //EPO2_ROBOT_STATE_H
