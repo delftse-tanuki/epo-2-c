330817b6113cfc720ff42595c5acabc117932402
Jens Heijenbrok <jensheijenbrok@hotmail.com>
2023-06-08 12:23:06 +0200

    UART refactor

--- src/backend/uart/uartHandler.h
+++ src/backend/uart/uartHandler.h
@@ -5,8 +5,10 @@
 #ifndef SRC_UARTHANDLER_H
 #define SRC_UARTHANDLER_H
 
+#include "UARTInstructions.h"
+
 void initUART();
-void executePath(struct Path path);
+void executePath(struct Path path, void (*path_ended)(enum PathExecutionResult));
 void closeConnection();
 
 #endif //SRC_UARTHANDLER_H
